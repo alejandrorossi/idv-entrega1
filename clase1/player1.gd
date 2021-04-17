c083a4137600588511604724b1b7b0d9d3591ca3
Alejandro Rossi <alejandro.rossi@frubana.com>
2021-04-16 21:31:11 -0300

    idv entrega 1

---
+++ clase1/player1.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+export (float) var speed:float
+
+func _process(delta):
+#	var right:bool = Input.is_action_pressed("derecha")
+#	var left:bool = Input.is_action_pressed("izquierda")
+	
+#	if right:
+#		position.x += speed * delta
+#	if left:
+#		position.x -= speed * delta
+	
+	var direction:int = int(Input.is_action_pressed("derecha")) - int(Input.is_action_pressed("izquierda"))
+	position.x += direction * speed * delta
